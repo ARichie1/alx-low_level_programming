99dad341b4a06e4824cc95dc4aaee4b1ba24b15e
arichie1 <richardakinmade3@gmail.com>
2022-09-09 16:39:08 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,20 +7,23 @@
  */
 int main(void)
 {
-	int tens;
-	int ones;
+	int num1, num2;
 
-	for (tens = 0; tens <= 9; tens++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (ones = 0; ones <= 9; ones++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar(tens + '0');
-			putchar(ones + '0');
-			if (!(tens == 9 && ones == 9))
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 98 && num2 == 99)
 			{
-				putchar(',');
-				putchar(' ');
+				continue
 			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
