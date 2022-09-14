a12285ef3372bee8c0e649cf1cd9bccf8ebe0b36
arichie1 <richardakinmade3@gmail.com>
2022-09-13 20:00:16 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,52 +8,44 @@
 
 void print_times_table(int n)
 {
-	int result;
+	int i, j, k;
 
-	if (n <= 15 && n > 0)
+	if (n >= 0 && n <= 15)
 	{
-		int i;
-
 		for (i = 0; i <= n; i++)
 		{
-			int j;
-
 			for (j = 0; j <= n; j++)
 			{
-				result = i * j;
+				k = j * i;
 				if (j == 0)
-				{
-					_putchar('0' + result);
+					_putchar(k + '0')
 				}
-				else if (result >= 10)
-				{
-					_putchar(' ');
-					_putchar('0' + (result / 10));
-					_putchar('0' + (result % 10));
-				}
-				else if (result >= 100)
-				{
-					_putchar(' ');
-					_putchar('0' + (result / 100));
-					_putchar('0' + ((result / 10) % 10));
-					_putchar('0' + (result % 10));
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-				if (j != n)
+				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k / 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
 		}
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
