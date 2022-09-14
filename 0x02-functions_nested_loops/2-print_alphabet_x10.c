3371ef6711fc41dd68d177f10a9c70f36ce4aac3
arichie1 <richardakinmade3@gmail.com>
2022-09-13 17:50:20 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code for ALX School students.
+ * Return: Alwys 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int b = 0;
+
+	while (b < 10)
+	{
+		char a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		b++;
+	}
+}
